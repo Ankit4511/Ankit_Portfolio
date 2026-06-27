7e48ced1d78509fd48c971a934d08ffc2dc58bf4
Ankitkumar Suthar <ankitsuthar683@gmail.com>
2026-06-27 22:51:26 +0530

    Fix CORS

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,6 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(healthRoutes);
 app.use(chatRoutes);
 
--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -2,24 +2,20 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://192.168.1.6:5173",
 
-  // Vercel Domains
   "https://ankit-portfolio-inky.vercel.app",
-  "https://ankit-portfolio-66tz6r8wq-ankit4511s-projects.vercel.app",
   "https://ankit-portfolio-git-main-ankit4511s-projects.vercel.app",
+  "https://ankit-portfolio-66tz6r8wq-ankit4511s-projects.vercel.app",
 ];
 
 const corsOptions = {
   origin(origin, callback) {
-    // Postman / Mobile Apps
-    if (!origin) {
-      return callback(null, true);
-    }
+    if (!origin) return callback(null, true);
 
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
 
-    return callback(new Error("Not allowed by CORS"));
+    callback(new Error("Not allowed by CORS"));
   },
 
   credentials: true,
